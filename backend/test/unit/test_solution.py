648c7aedfc43bd5b14893e0a2fdd99b6d7141df0
David Dahlgren <david.dahlgren@gmail.com>
2024-05-20 18:55:24 +0200

    test

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,6 +1,12 @@
 import pytest
+import unittest.mock as mock #Load the mocking library
+from src.controllers.recipecontroller import RecipeController
 
 # add your test case implementation here
 @pytest.mark.unit
 def test():
-    pass
+    controller = mock.MagicMock() #Mock the dependency
+    sut = RecipeController(controller)
+
+    recipies = sut.load_recipes()
+    assert len(recipies) == 2
